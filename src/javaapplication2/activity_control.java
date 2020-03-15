721a9dba8223d8d3559dadd9defd718a77f49b16
Dante Giuliano <>
2020-03-15 17:20:43 -0300

    este programa pretende realizar trazas de un modelo computacional de como interpretan las instrucciones la ALU , consta de una carga de datos que se almacenar de forma bidimensional. y realizan el conjunto de instrucciones basicas.

---
+++ src/javaapplication2/activity_control.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Dantesito
+ */
+public class activity_control {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       activity_Principal dante = new activity_Principal();
+       dante.setVisible(true);
+       }
+    
+    
+}
